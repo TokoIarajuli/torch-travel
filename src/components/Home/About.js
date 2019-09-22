a6f197cdddcd27701e286e5c7348461d71417af8
root <root@localhost.localdomain>
2019-09-22 17:55:24 +0400

    Added services page

--- src/components/Home/About.js
+++ src/components/Home/About.js
@@ -1,12 +1,32 @@
 import React from 'react'
 import Title from '../Title'
+import styles from '../../css/about.module.css'
+import img from '../../images/defaultBcg.jpeg'
 
 const About = () => {
   return (
-    <div>
-      Hello from About afsfa
-      <Title />
-    </div>
+    <section className={styles.about}>
+      <Title title="about" subtitle="us" />
+      <div className={styles.aboutCenter}>
+        <article className={styles.aboutImg}>
+          <img src={img} alt="About Company" />
+        </article>
+        <article className={styles.aboutInfo}>
+          <h4>Explore the difference</h4>
+          <p>
+            Torch Torch Torch Torch Torch Torch Torch Torch Torch Torch Torch
+            Torch Torch Torch
+          </p>
+          <p>
+            Torch Torch Torch Torch Torch Torch Torch Torch Torch Torch Torch
+            Torch Torch Torch
+          </p>
+          <button type="button" className="btn-primary">
+            Read more
+          </button>
+        </article>
+      </div>
+    </section>
   )
 }
 
